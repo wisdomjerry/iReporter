e9a36f584492b1902bbd7b76af8b5e80e4bc969f
wisdomjerry <wisdom.jeremiah.upti@gmail.com>
2026-01-08 20:13:41 +0300

    add socket.io code supabase key

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -1,11 +1,14 @@
 // supabaseClient.js
 const { createClient } = require("@supabase/supabase-js");
-require("dotenv").config();
 
+console.log("ENV CHECK", {
+  SUPABASE_URL: !!process.env.SUPABASE_URL,
+  SUPABASE_SERVICE_ROLE_KEY: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
+});
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY // use service_role here
+  process.env.SUPABASE_SERVICE_ROLE_KEY // use service_role here
 );
 
 module.exports = supabase;
